50789702c0278d00e29871030d504bbedad5e49d
lsfernandes92 <lsfernandes92@gmail.com>
2021-10-26 14:44:04 -0300

    Add Stock Picker basic ruby project

---
+++ basic_ruby_projects/stock_picker/spec/stock_profit_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require_relative '../stock_profit'
+
+RSpec.describe StockProfit do
+  subject { described_class.new(stock_prices) }
+
+  describe '#picker' do
+    let(:stock_prices) { [17,3,6,9,15,8,6,1,10] }
+
+    it 'returns a pair of days representing the best day to buy and sell' do
+      expect(StockProfit.new(stock_prices).picker).to eq([1, 4])
+    end
+
+    context 'when the best day to buy is the last day' do
+      let(:stock_prices) { [17,20,6,9,15,8,7,3,1] }
+
+      it 'returns a pair of days representing the best day to buy and sell' do
+        expect(StockProfit.new(stock_prices).picker).to eq([2, 4])
+      end
+    end
+
+    context 'when the best day to sell is the first day' do
+      let(:stock_prices) { [20,17,6,9,15,8,7,3,1] }
+
+      it 'returns a pair of days representing the best day to buy and sell' do
+        expect(StockProfit.new(stock_prices).picker).to eq([2, 4])
+      end
+    end
+  end
+end
---
+++ basic_ruby_projects/stock_picker/stock_profit.rb
@@ -0,0 +1,46 @@
+class StockProfit
+  attr_reader :stock_prices
+
+  def initialize(stock_prices)
+    @stock_prices = stock_prices
+    @profit = 0
+    @best_buy_day = 0
+    @best_sell_day = 0
+  end
+
+  def picker
+    sorted_stock_prices.each do |min_price|
+      stock_prices[stock_price_day(min_price)...stock_prices_length].each do |max_price|
+        if better_profit?(max_price, min_price)
+          @profit = calculate_profit(max_price, min_price)
+          @best_buy_day = stock_price_day(min_price)
+          @best_sell_day = stock_price_day(max_price)
+        end
+      end
+    end
+
+    [@best_buy_day, @best_sell_day]
+  end
+
+  private
+
+  def sorted_stock_prices
+    stock_prices.dup.sort
+  end
+
+  def stock_prices_length
+    stock_prices.length
+  end
+
+  def stock_price_day(stock)
+    stock_prices.index(stock)
+  end
+
+  def calculate_profit(max_price, min_price)
+    max_price - min_price
+  end
+
+  def better_profit?(max_price, min_price)
+    (max_price - min_price) > @profit
+  end
+end
