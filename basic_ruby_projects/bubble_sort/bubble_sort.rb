2a3337e75bd57a6131301ee3df5838c9b027f583
lsfernandes92 <lsfernandes92@gmail.com>
2021-10-27 21:07:46 -0300

    Add Bubble Sorte basic ruby project

---
+++ basic_ruby_projects/bubble_sort/bubble_sort.rb
@@ -0,0 +1,22 @@
+class BubbleSort
+  class << self
+    def sort(numbers)
+      raise 'Please only numbers can be sorted' unless numbers.is_a?(Array)
+      unsorted = true
+
+      while unsorted do
+        unsorted = false
+        (0..numbers.size - 1).each do |i|
+          next if i == numbers.size - 1
+
+          if numbers[i] > numbers[i.succ]
+            numbers[i], numbers[i.succ] = numbers[i.succ], numbers[i]
+            unsorted = true
+          end
+        end
+      end
+
+      numbers
+    end
+  end
+end
---
+++ basic_ruby_projects/bubble_sort/spec/bubble_sort_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+require_relative '../bubble_sort'
+
+RSpec.describe BubbleSort do
+  context '#sort' do
+    context 'with numbers' do
+      let(:numbers) { [4,3,78,2,0,2] }
+
+      it 'returns a sorted array' do
+        expect(BubbleSort.sort(numbers)).to eq([0,2,2,3,4,78])
+      end
+    end
+
+    context 'with letters' do
+      let(:numbers) { %w[d a c b] }
+
+      it 'returns a sorted array' do
+        expect(BubbleSort.sort(numbers)).to eq(%w[a b c d])
+      end
+    end
+
+    context 'with invalid input' do
+      it 'raises and error ' do
+        expect{ BubbleSort.sort('foo') }.to raise_error('Please only numbers can be sorted')
+      end
+    end
+  end
+end
