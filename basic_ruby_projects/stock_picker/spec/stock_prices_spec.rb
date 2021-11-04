5231189127f701bd6f5ecef08523ad86bd5f536a
lsfernandes92 <lsfernandes92@gmail.com>
2021-11-04 15:47:40 -0300

    Run rubocop formatter

--- basic_ruby_projects/stock_picker/spec/stock_prices_spec.rb
+++ basic_ruby_projects/stock_picker/spec/stock_prices_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe StockPrices do
   subject { described_class.new(stock_prices) }
 
   describe '#profit_picker' do
-    let(:stock_prices) { [17,3,6,9,15,8,6,1,10] }
+    let(:stock_prices) { [17, 3, 6, 9, 15, 8, 6, 1, 10] }
 
     it 'returns a pair of days representing the best day to buy and sell' do
       expect(StockPrices.new(stock_prices).profit_picker).to eq([1, 4])
     end
 
     context 'when the best day to buy is the last day' do
-      let(:stock_prices) { [17,20,6,9,15,8,7,3,1] }
+      let(:stock_prices) { [17, 20, 6, 9, 15, 8, 7, 3, 1] }
 
       it 'returns a pair of days representing the best day to buy and sell' do
         expect(StockPrices.new(stock_prices).profit_picker).to eq([2, 4])
@@ -20,7 +20,7 @@ RSpec.describe StockPrices do
     end
 
     context 'when the best day to sell is the first day' do
-      let(:stock_prices) { [20,17,6,9,15,8,7,3,1] }
+      let(:stock_prices) { [20, 17, 6, 9, 15, 8, 7, 3, 1] }
 
       it 'returns a pair of days representing the best day to buy and sell' do
         expect(StockPrices.new(stock_prices).profit_picker).to eq([2, 4])
